438ec71f0827cf5f8f3ddce1ba829adae0a09fde
jevantenaglia <jtenaglia@ufl.edu>
2023-04-17 19:39:36 -0400

    screen

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,15 @@
 from sudoku_generator import SudokuGenerator
-import pygame
+import pygame, sys
 
+pygame.init()
+pygame.display.set_caption("Sudoku")
+screen = pygame.display.set_mode((600,600))
+
+while True:
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        pygame.display.update()
