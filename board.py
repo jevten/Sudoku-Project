e519be73addc36e744b996d9471402abb38ebb90
jevantenaglia <jtenaglia@ufl.edu>
2023-04-18 15:36:02 -0400

    working on sudoku gui

--- board.py
+++ board.py
@@ -14,11 +14,11 @@ class Board:
         self.LINE_COLOR_CELL = (242, 158, 22)
 
     def draw(self):
+        for i in range(1,9):
+            pygame.draw.line(self.screen, self.LINE_COLOR_CELL, (0,i*600/9),(self.width, i*600/9),2)
+        for i in range(1,9):
+            pygame.draw.line(self.screen,self.LINE_COLOR_CELL,(i * 600/9,0), (600/9 * i, self.height),3)
         for i in range(1, 4):
             pygame.draw.line(self.screen, self.LINE_COLOR_BOX, (0, i * self.SQUARE_SIZE_VERT), (self.width, i * self.SQUARE_SIZE_VERT), 5)
         for i in range(1, 3):
             pygame.draw.line(self.screen, self.LINE_COLOR_BOX, (i * self.SQUARE_SIZE_HORZ,0), (self.SQUARE_SIZE_HORZ * i, self.height), 5)
-
-
-
-
